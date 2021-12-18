eb1c3d704bf2e22d0d8077ffa6e0ea327ebdac73
Neeyat Lotlikar <neeyat.lotlikar@gmail.com>
2021-12-18 13:04:16 +0530

    Footer implemented

--- landing.php
+++ landing.php
@@ -20,7 +20,7 @@
 		<title>Coding School - Landing Page</title>
 		<meta name="description" content="">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="">
+		<link rel="stylesheet" href="public/styles/style.css">
 	</head>
 	<body>
         <h1>This shall be my project page!</h1>
--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,44 @@
-<div class="footer">
-    <p>This will be the footer.</p>
-</div>
+<footer>
+    <div class="footer">
+
+        <div class="left">
+            <h3>GrowthGravy</h3>
+            <p>Bringing 21st century skills to <br>K-12 students worldwide</p>
+            <div class="social-links">
+
+            </div>
+        </div>
+        <div class="r1">
+            <h3>Online Courses</h3>
+            <p>Artificial Intelligence</p>
+            <p>Virtual Reality</p>
+            <p>Web Development</p>
+            <p>Android Development</p>
+            <p>Explore All</p>
+        </div>
+        <div class="r2">
+            <h3>Company</h3>
+            <p>About Us</p>
+            <p>Our Founder</p>
+            <p>Testimonials</p>
+            <p>Jobs</p>
+            <p>Teach With Us</p>
+        </div>
+
+        <!-- Horizontal Rule -->
+        <div class="hr">
+            <hr>
+        </div>
+
+        <!-- Lower Part -->
+        <div class="copyrights">
+            <p>(C) 2019 Camp K12. All rights reserved.</p>
+        </div>
+        <div class="br1">
+            <p>Terms and Conditions</p>
+        </div>
+        <div class="br2">
+            <p>Privacy Policy</p>
+        </div>
+    </div>
+</footer>
