656286f5376d2b5c86514df92f3f3b507e5704fa
Neeyat Lotlikar <neeyat.lotlikar@gmail.com>
2021-12-18 16:00:55 +0530

    Header content added.

--- partials/header.php
+++ partials/header.php
@@ -1,3 +1,36 @@
 <div class="header">
-    <h1>Please work! Please! Please!</h1>
+
+    <ul class="topnav">
+        <li><a><b>GROWTHGRAVY</b></a></li>
+        <li>
+            <hr>
+        </li>
+        <li>
+            <form action="" method="get">
+                <select name="type">
+                    <option value="coding">CODING</option>
+                </select>
+            </form>
+        </li>
+        <li><a href="#reviews">REVIEWS</a></li>
+        <li><a href="#community">COMMUNITY</a></li>
+        <li><a href="#whyus">WHY US?</a></li>
+        <li><a href="#more">MORE</a></li>
+        <li><button>CHAMP</button></li>
+        <li><button>FREE TRIAL</button></li>
+        <li><button>LOG IN</button></li>
+    </ul>
+
+    <div class="grid-content">
+        <div class="left">
+            <h1>Global Online School for<br>21st Century Skills</h1>
+            <p>💻 LIVE online classes, learn 1:1 or with friends</p>
+            <p>📚 Coding, Math, and English for Ages 5 - 18</p>
+            <p>🌟 World's best instructors, in the comfort of home</p>
+        </div>
+        <div class="right">
+            <img src="public/images/kid-tv-learn.jpg" alt="Kid learning on Tablet" width="400px" height="400px">
+        </div>
+    </div>
+
 </div>
