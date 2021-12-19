b1a038428cda24e0c60fc3e0559646c8d7f7c1ab
Neeyat Lotlikar <neeyat.lotlikar@gmail.com>
2021-12-19 16:26:12 +0530

    Added a basic structure to classroom-experience and added cards to it.

--- partials/classroom-experience.php
+++ partials/classroom-experience.php
@@ -1,11 +1,38 @@
 <div class="classroom-exp">
     <h1>A new classroom experience for<br>parents & kids</h1>
     <div class="grid-content">
+
         <div class="left">
-            <!-- A few cards will come here. -->
+
+            <div class="card card-content">
+                <p><b>Live 1:1 classes with personalized instruction</b></p>
+                <p>
+                    Interactive, engaging and fun classes from the<br>comforts of home. 
+                    Choose a flexible schedule,<br>any day / any time.
+                </p>
+            </div>
+
+            <div class="card card-content">
+                <p><b>Top 1% teachers from across the globe,</b> trained by MIT / Harvard CS alumni</p>
+            </div>
+
+            <div class="card card-content">
+                <p><b>11+ years of Curriculum<br> Excellence:</b> learn math & coding<br>via real-world projects</p>
+            </div>
+
+            <div class="card card-content">
+                <p><b>Parent app with<br>unprecedented visibility into<br>your child's growth</b></p>
+            </div>
+
+            <div class="card card-content">
+                <p><b>Dedicated guidance couselor<br>and regular Parent-Teachers<br>meetings</b></p>
+            </div>
+
         </div>
+
         <div class="right">
-            <img src="public/images/programming.jpg" alt="A man programming" width="300px" height="200px">
+            <img src="public/images/programming.jpg" alt="A man programming" width="600px" height="400px">
         </div>
+
     </div>
 </div>
