f8951e8595a99a110e25ec879afeaefd29d51eeb
Neeyat Lotlikar <neeyat.lotlikar@gmail.com>
2021-12-19 18:54:28 +0530

    Improved Classroom Experience.

--- partials/classroom-experience.php
+++ partials/classroom-experience.php
@@ -4,27 +4,32 @@
 
         <div class="left">
 
-            <div class="card card-content">
+            <div class="card card-content special">
+                <img src="https://img.icons8.com/fluency/96/000000/knowledge-sharing.png" />
                 <p><b>Live 1:1 classes with personalized instruction</b></p>
                 <p>
-                    Interactive, engaging and fun classes from the<br>comforts of home. 
+                    Interactive, engaging and fun classes from the<br>comforts of home.
                     Choose a flexible schedule,<br>any day / any time.
                 </p>
             </div>
 
             <div class="card card-content">
+                <img src="https://img.icons8.com/fluency/96/000000/knowledge-sharing.png" />
                 <p><b>Top 1% teachers from across the globe,</b> trained by MIT / Harvard CS alumni</p>
             </div>
 
             <div class="card card-content">
+                <img src="https://img.icons8.com/fluency/96/000000/knowledge-sharing.png" />
                 <p><b>11+ years of Curriculum<br> Excellence:</b> learn math & coding<br>via real-world projects</p>
             </div>
 
             <div class="card card-content">
+                <img src="https://img.icons8.com/fluency/96/000000/knowledge-sharing.png" />
                 <p><b>Parent app with<br>unprecedented visibility into<br>your child's growth</b></p>
             </div>
 
             <div class="card card-content">
+                <img src="https://img.icons8.com/fluency/96/000000/knowledge-sharing.png" />
                 <p><b>Dedicated guidance couselor<br>and regular Parent-Teachers<br>meetings</b></p>
             </div>
 
