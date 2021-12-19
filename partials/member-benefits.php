15493b2ac4434f71297878211231e564ac266a6e
Neeyat Lotlikar <neeyat.lotlikar@gmail.com>
2021-12-19 16:07:11 +0530

    Added member benefits table, the start of it.

--- partials/member-benefits.php
+++ partials/member-benefits.php
@@ -1,3 +1,108 @@
 <div class="member-benefits">
     <h1>Membership Benefits</h1>
+
+    <button class="g12">Grade 1-2</button>
+    <button class="g34">Grade 3-4</button>
+    <button class="g56">Grade 5-6</button>
+    <button class="g78">Grade 7-8</button>
+    <button class="g9tw">Grade 9-12</button>
+
+    <table border="1px" align="center">
+        <tr>
+            <th>
+                <!-- Required Empty Table Header -->
+            </th>
+            <th>
+                <h4>BEGINNER</h4>
+            </th>
+            <th>
+                <h4>ADVANCED</h4>
+            </th>
+            <th>
+                <h4>MASTERY</h4>
+            </th>
+        </tr>
+        <tr>
+            <td>⌚ Tenure</td>
+            <td>~3 months</td>
+            <td>~6 months</td>
+            <td>~12 months</td>
+        </tr>
+        <tr>
+            <td>💻 1:1 live online classes<br>(instructor of your choice)</td>
+            <td>36 classes</td>
+            <td>72 classes</td>
+            <td>144 classes</td>
+        </tr>
+        <tr>
+            <td>💻 1:1 live online classes<br>(instructor of your choice)</td>
+            <td>36 classes</td>
+            <td>72 classes</td>
+            <td>144 classes</td>
+        </tr>
+    
+        <tr>
+            <td>💻 1:1 live online classes<br>(instructor of your choice)</td>
+            <td>36 classes</td>
+            <td>72 classes</td>
+            <td>144 classes</td>
+        </tr>
+    
+        <tr>
+            <td>💻 1:1 live online classes<br>(instructor of your choice)</td>
+            <td>36 classes</td>
+            <td>72 classes</td>
+            <td>144 classes</td>
+        </tr>
+    
+        <tr>
+            <td>💻 1:1 live online classes<br>(instructor of your choice)</td>
+            <td>36 classes</td>
+            <td>72 classes</td>
+            <td>144 classes</td>
+        </tr>
+    
+        <tr>
+            <td>💻 1:1 live online classes<br>(instructor of your choice)</td>
+            <td>36 classes</td>
+            <td>72 classes</td>
+            <td>144 classes</td>
+        </tr>
+    
+        <tr>
+            <td>💻 1:1 live online classes<br>(instructor of your choice)</td>
+            <td>36 classes</td>
+            <td>72 classes</td>
+            <td>144 classes</td>
+        </tr>
+    
+        <tr>
+            <td>💻 1:1 live online classes<br>(instructor of your choice)</td>
+            <td>36 classes</td>
+            <td>72 classes</td>
+            <td>144 classes</td>
+        </tr>
+    
+        <tr>
+            <td>💻 1:1 live online classes<br>(instructor of your choice)</td>
+            <td>36 classes</td>
+            <td>72 classes</td>
+            <td>144 classes</td>
+        </tr>
+    
+        <tr>
+            <td>💻 1:1 live online classes<br>(instructor of your choice)</td>
+            <td>36 classes</td>
+            <td>72 classes</td>
+            <td>144 classes</td>
+        </tr>
+    
+        <tr>
+            <td>💻 1:1 live online classes<br>(instructor of your choice)</td>
+            <td>36 classes</td>
+            <td>72 classes</td>
+            <td>144 classes</td>
+        </tr>
+    </table>
+
 </div>
