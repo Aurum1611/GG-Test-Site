a4de1b872e020e1a4356a9c0d45275a1b7d1b46c
Neeyat Lotlikar <neeyat.lotlikar@gmail.com>
2021-12-19 19:38:37 +0530

    Made improvements to learning paths section.

--- partials/learning-paths.php
+++ partials/learning-paths.php
@@ -1,7 +1,7 @@
 <div class="learn-paths">
     <h1>Long-term learning paths, personalized<br>for your child.</h1>
     <p>
-        With 11+ years of experience teaching kids to code, we've built a hands on curriculum 
+        With 11+ years of experience teaching kids to code, we've built a hands on curriculum
         for every age, with<br>real-world projects your kids will love and won't find anywhere else.
     </p>
 
@@ -12,17 +12,37 @@
         <button class="g78">Grade 7-8</button>
         <button class="g9tw">Grade 9-12</button>
 
-        <h3>3 months</h3>
-        <p>(36 classes)</p>
+        <div class="grid-container">
 
-        <h3>6 months</h3>
-        <p>(36 classes)</p>
+            <div class="item">
+                <img width="300px" height="250px" src="public/images/game-play.png" alt="Game Development">
+                <h3>Game Dev<br>with AI</h3>
+                <h3>3 months</h3>
+                <p>(36 classes)</p>
+            </div>
 
-        <h3>9 months</h3>
-        <p>(36 classes)</p>
+            <div class="item">
+                <img width="300px" height="250px" src="public/images/virtual-reality.png" alt="Virtual Reality">
+                <h3>3D Game<br>with VR</h3>
+                <h3>6 months</h3>
+                <p>(36 classes)</p>
+            </div>
 
-        <h3>12 months</h3>
-        <p>(36 classes)</p>
+            <div class="item">
+                <img width="300px" height="250px" src="public/images/mobile-app.png" alt="Mobile App">
+                <h3>Mobile App<br>Development</h3>
+                <h3>9 months</h3>
+                <p>(36 classes)</p>
+            </div>
+
+            <div class="item">
+                <img width="300px" height="250px" src="public/images/flame-ai.png" alt="AI">
+                <h3>AI Chatbots<br>& Voice Ass.</h3>
+                <h3>12 months</h3>
+                <p>(36 classes)</p>
+            </div>
+
+        </div>
 
     </div>
 
