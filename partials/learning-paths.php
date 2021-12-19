85baca23dbaf65322e41de6816e91a97827810ae
Neeyat Lotlikar <neeyat.lotlikar@gmail.com>
2021-12-19 15:50:07 +0530

    Little bit learning-paths setup.

--- partials/learning-paths.php
+++ partials/learning-paths.php
@@ -1,3 +1,31 @@
 <div class="learn-paths">
     <h1>Long-term learning paths, personalized<br>for your child.</h1>
+    <p>
+        With 11+ years of experience teaching kids to code, we've built a hands on curriculum 
+        for every age, with<br>real-world projects your kids will love and won't find anywhere else.
+    </p>
+
+    <div class="plan">
+        <button class="g12">Grade 1-2</button>
+        <button class="g34">Grade 3-4</button>
+        <button class="g56">Grade 5-6</button>
+        <button class="g78">Grade 7-8</button>
+        <button class="g9tw">Grade 9-12</button>
+
+        <h3>3 months</h3>
+        <p>(36 classes)</p>
+
+        <h3>6 months</h3>
+        <p>(36 classes)</p>
+
+        <h3>9 months</h3>
+        <p>(36 classes)</p>
+
+        <h3>12 months</h3>
+        <p>(36 classes)</p>
+
+    </div>
+
+    <button>BOOK A FREE TRIAL CLASS</button>
+    <button>DOWNLOAD CURRICULUM</button>
 </div>
